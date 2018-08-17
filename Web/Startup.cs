0bad3abba6a1b01fdd70e1d9bcd3d870b806f5ce
pixelbits <pixelbits100@gmail.com>
2018-08-17 01:54:26 -0600

    updated

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -63,7 +63,7 @@ namespace Web
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                //c.RoutePrefix = string.Empty;
+                c.RoutePrefix = "api";
             });
 
             app.UseDefaultFiles();
