97d3b26a44736e79bf498fcaebd7fd281d288ede
pixelbits <pixelbits100@gmail.com>
2018-08-19 22:26:51 -0600

    updated

--- Schemas/RootMutation.cs
+++ Schemas/RootMutation.cs
@@ -10,13 +10,15 @@ namespace Schemas
     {
         public RootMutation(ShoppingCartService service)
         {
-            Field<ShoppingCartType>("createCart",
+            Field<ShoppingCartType>(
+                "createCart",
                 resolve: ctx =>
                 {
                     return service.StartNewCart();
                 });
 
-            Field<BooleanGraphType>("deleteCart",
+            Field<BooleanGraphType>(
+                "deleteCart",
                 arguments: new QueryArguments(
                     new QueryArgument<IntGraphType>() { Name = "id" }
                 ),
@@ -25,6 +27,21 @@ namespace Schemas
                     var cartId = ctx.GetArgument<int>("id");
                     service.AbandonCart(cartId);
                     return true;
+                });
+
+            Field<ShoppingCartItemType>(
+                "addCartItem",
+                arguments: new QueryArguments(
+                    new QueryArgument<IntGraphType>() {  Name = "shoppingCartId" },
+                    new QueryArgument<IntGraphType>() {  Name = "productId"},
+                    new QueryArgument<IntGraphType>() {  Name = "quantity"}
+                ),
+                resolve: ctx =>
+                {
+                    var cartId = ctx.GetArgument<int>("shoppingCartId");
+                    var productId = ctx.GetArgument<int>("productId");
+                    var quantity = ctx.GetArgument<int>("quantity");
+                    return service.AddItem(cartId, productId, quantity);
                 });
 
         }
